7c556b8286726370c671cfe794301d3866735c6e
sumin <suminkim.me@gmail.com>
2022-09-17 18:05:14 +0900

    exerist 페이지 추가

---
+++ pages/exerist/index.tsx
@@ -0,0 +1,12 @@
+import GlobalLayout from '@common/bridges/GlobalLayout';
+import Exerist from '@specifics/exerist/bridges';
+
+function ExeristPage() {
+  return (
+    <GlobalLayout>
+      <Exerist />
+    </GlobalLayout>
+  );
+}
+
+export default ExeristPage;
---
+++ specifics/exerist/bridges/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Typography, Table, Chip } from '@mui/material';
+
+function Exerist() {
+  const hi = 'Chest';
+  return (
+    <Box>
+      <Typography>캘린더 영역</Typography>
+      <Box>
+        <Typography>리스트 영역</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default Exerist;
