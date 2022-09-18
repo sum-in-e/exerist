be723f6ad7ef8951105bebc80ad33c8758a2a189
sumin <suminkim.me@gmail.com>
2022-09-18 14:52:43 +0900

    헤더 로고 사이즈 변경

--- common/bridges/GlobalHeader.tsx
+++ common/bridges/GlobalHeader.tsx
@@ -3,7 +3,7 @@ import { Avatar, Box, Divider, Typography } from '@mui/material';
 function GlobalHeader() {
   return (
     <Box display="flex" justifyContent="center" alignItems="center" p={2}>
-      <Typography variant="h4" color="#3b9ae1" fontWeight="bold">
+      <Typography variant="h5" color="#3b9ae1" fontWeight="bold">
         EXERIST
       </Typography>
     </Box>
